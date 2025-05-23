06fee8ac7c715716f11cd266410ffbfc050aff38
Boru Isako <isco.jaldy@gmail.com>
2025-05-23 14:53:54 +0300

    Complete labs

---
+++ app/migrations/versions/258ee4c461b8_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 258ee4c461b8
+Revises: 
+Create Date: 2025-05-23 14:47:47.099517
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '258ee4c461b8'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
